c55c8e7c9b1b6134eae780820ea91d848ac2350c
hanniel <hanniel.hu@gmail.com>
2016-09-20 01:09:05 +0200

    Delete SVGanimation.js

--- SVGanimation.js
+++
@@ -1,7 +0,0 @@
-TweenMax.to(".mobile", 2, { scale:1.5, y:12, ease:Power4.easeIN});
-TweenMax.to(".screen", 2, { scale:1.5, y:12, ease:Power4.easeIN});
-TweenMax.to(".speaker", 2, { scale:1.5, y:12, ease:Power4.easeIN});
-TweenMax.to(".logo", 0.5, { y:-100, ease:Power4.easeIN});
-TweenMax.to(".button", 2, { scale:1.5, y:150,x:130, ease:Power4.easeIN});
-TweenMax.to(".shadow", 2, { scale:1.5, y:67, ease:Power4.easeIN});
-TweenMax.to(".logo", 0.8, { y:100, ease:Power4.easeIN});
